97489e241e173efc64fb7a48bf1f14e645563cf5
Tarunvenigalla <140583320+Tarunvenigalla@users.noreply.github.com>
2023-07-31 09:35:38 +0530

    code changed

--- weather-app.py
+++ weather-app.py
@@ -1,15 +1,23 @@
 import requests
-api_key=('ce9259e1b5be0bd1d89fc9a1ffde119e')
-city=input('Enter City:')
-url=f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-response=requests.get(url)
-if response.status_code==200:
+
+def weather(city,api_key):
+    url=f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response=requests.get(url)
     data=response.json()
-    temp=data['main']['temp']
-    temp_ce=temp-273.15
-    desc = data['weather'][0]['description']
-    print(f"Temperature: {int(temp_ce)}'C")
-    print(f'Description: {desc}')
-else:
-    print('Error fetching weather data')
-    
+    if data['cod']!=404:
+        main_data=data['main']
+        weather_data=data['weather'][0]
+        weather_description=weather_data['description']
+        temperature_data=main_data['temp']
+        humidity_data=main_data['humidity']
+        
+        print(f'weather in {city}:{weather_description}')
+        print(f'Temperature:{int(temperature_data-273.15)}°C')
+        print(f'Hunidity:{humidity_data}%')
+    else:
+        print('City not found...')
+
+if __name__=='__main__':
+    api_key=('ce9259e1b5be0bd1d89fc9a1ffde119e')
+    city=input('Enter City:')
+    weather(city,api_key)
